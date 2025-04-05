9d69ee673837ce3c6cf76220d58004479f44d594
Caio Cavalcante <caiocavalcantedev@gmail.com>
2025-04-05 02:48:21 -0300

    add: aula 2 finalizada

--- mentorados/admin.py
+++ mentorados/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Mentorados, Navigators
+from .models import Mentorados, Navigators, Reuniao, DisponibilidadeHorarios
 
 admin.site.register(Navigators)
 admin.site.register(Mentorados)
+admin.site.register(Reuniao)
+admin.site.register(DisponibilidadeHorarios)
